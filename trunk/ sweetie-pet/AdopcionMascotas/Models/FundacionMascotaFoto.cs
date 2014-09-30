a8e4d4fb5d7385c51a113e5aba7e3f3380ce60b6
leebeth.anacona@gmail.com <leebeth.anacona@gmail.com@a5445092-6ae7-8c19-d427-cb3ff2d24d8f>
2014-09-30 03:37:45 +0000

    Maestro detalle detalle. Fundacion Mascota Foto

---
+++ trunk/ sweetie-pet/AdopcionMascotas/Models/FundacionMascotaFoto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdopcionMascotas.Models
+{
+    public class FundacionMascotaFoto
+    {
+        public IEnumerable<Fundación> Fundaciones { get; set; }
+
+        public IEnumerable<Mascota> Mascotas { get; set; }
+
+        public IEnumerable<Foto> Fotos { get; set; }
+    }
+}
