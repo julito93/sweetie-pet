9be3c590590a8cc58c3ab23c070d0ab5608824d4
miguelpiza930@gmail.com <miguelpiza930@gmail.com@a5445092-6ae7-8c19-d427-cb3ff2d24d8f>
2014-11-26 19:53:39 +0000

    - cuando una fundacion crea una mascota, esta queda vinculada a la fundacion que hizo login - para una fundacion solo muestra las mascotas que pertenecen a esa fundacion

--- trunk/ sweetie-pet/AdopcionMascotas/App_Start/IdentityConfig.cs
+++ trunk/ sweetie-pet/AdopcionMascotas/App_Start/IdentityConfig.cs
@@ -37,10 +37,10 @@ namespace IdentitySample.Models
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
