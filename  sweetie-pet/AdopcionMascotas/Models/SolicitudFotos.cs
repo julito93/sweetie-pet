63cb7d16bd6495cd56483560d9bc53953b44d94d
miguelpiza930@gmail.com <miguelpiza930@gmail.com@a5445092-6ae7-8c19-d427-cb3ff2d24d8f>
2014-11-27 20:54:52 +0000

    maestro detalle en solicitud de un padre y muestra fotos de la Mascota

---
+++  sweetie-pet/AdopcionMascotas/Models/SolicitudFotos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdopcionMascotas.Models
+{
+    public class SolicitudFotos
+    {
+        public IEnumerable<SolicitudAdopcion> Solicitudes { get; set; }
+        public IEnumerable<Foto> Fotos { get; set; }
+    }
+}
