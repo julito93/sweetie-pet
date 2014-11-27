785e4f020952c825dcdf2eece6245eb671a1d966
miguelpiza930@gmail.com <miguelpiza930@gmail.com@a5445092-6ae7-8c19-d427-cb3ff2d24d8f>
2014-11-27 05:51:47 +0000

    mal redireccion de adoptar a crearsolicitud cuando ya tiene rol, pero si no lo tiene crea el rol, lo asigna y crea la solicitud bien, y la mascota queda con su adopcion asociada, el estado es sin aprobar

---  sweetie-pet/AdopcionMascotas/Models/SolicitudAdopcion.cs
+++  sweetie-pet/AdopcionMascotas/Models/SolicitudAdopcion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,7 @@ namespace AdopcionMascotas.Models
     {
         public int ID { get; set; }
         public string PadreAdoptivo_Cedula { get; set; }
-        public DateTime Fecha_Adop { get; set; }
+        public string Fecha_Adop { get; set; }
         public string Estado { get; set; }
 
         public virtual PadreAdoptivo PadreAdoptivo {get; set;}
