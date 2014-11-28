ab4af1cbdafc06618b2f6a0c544b2ea3146d568c
miguelpiza930@gmail.com <miguelpiza930@gmail.com@a5445092-6ae7-8c19-d427-cb3ff2d24d8f>
2014-11-28 06:53:35 +0000

    carrusel mascotas adoptadas ok

---  sweetie-pet/AdopcionMascotas/Controllers/HomeController.cs
+++  sweetie-pet/AdopcionMascotas/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
 ﻿using System.Web.Mvc;
+using System.Linq;
+using System.Data.Entity;
+using IdentitySample.Models;
+using System.Collections.Generic;
+using AdopcionMascotas.Models;
 
 namespace IdentitySample.Controllers
 {
@@ -6,9 +11,13 @@ namespace IdentitySample.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            List<Foto> fotos = db.SolicitudAdopcions.Where(s => s.Estado.Equals("Aprobado")).Select(s => s.Mascota.Fotos.FirstOrDefault()).Include(s => s.Mascota).ToList();
+            return View(fotos);
         }
 
         public ActionResult About()
